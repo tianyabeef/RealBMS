b2bea41ebad09a921a532bae189e5c2fafb68c3d
zjh <love949872618@qq.com>
2018-10-30 14:48:42 +0800

    增加sql语句

---
+++ main_auth_group.sql
@@ -0,0 +1,8 @@
+INSERT INTO auth_group (id, name) VALUES (1, '实验部');
+INSERT INTO auth_group (id, name) VALUES (2, '项目管理');
+INSERT INTO auth_group (id, name) VALUES (3, '业务员（销售）');
+INSERT INTO auth_group (id, name) VALUES (4, '市场部');
+INSERT INTO auth_group (id, name) VALUES (5, '财务部');
+INSERT INTO auth_group (id, name) VALUES (6, '业务员（公司）');
+INSERT INTO auth_group (id, name) VALUES (7, '销售总监');
+INSERT INTO auth_group (id, name) VALUES (8, '合作伙伴');
