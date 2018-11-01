90aeb25a6aef30a789a4403e4a3eba35a41d5505
huangyun <410378266@qq.com>
2018-11-01 10:50:27 +0800

    更新了pm的SubProjectAdmin的过滤器，list_display,get_query()

--- main_auth_group.sql
+++ main_auth_group.sql
@@ -1,9 +1,15 @@
-INSERT INTO auth_group (id, name) VALUES (1, '实验部');
-INSERT INTO auth_group (id, name) VALUES (2, '项目管理');
-INSERT INTO auth_group (id, name) VALUES (3, '业务员（销售）');
-INSERT INTO auth_group (id, name) VALUES (4, '市场部');
-INSERT INTO auth_group (id, name) VALUES (5, '财务部');
-INSERT INTO auth_group (id, name) VALUES (6, '业务员（公司）');
-INSERT INTO auth_group (id, name) VALUES (7, '销售总监');
-INSERT INTO auth_group (id, name) VALUES (8, '合作伙伴');
-INSERT INTO auth_group (id, name) VALUES (9, '生物信息部');
+CREATE TABLE auth_group
+(
+    id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name varchar(80) NOT NULL
+);
+CREATE UNIQUE INDEX sqlite_autoindex_auth_group_1 ON auth_group (name);
+INSERT INTO auth_group (name) VALUES ('实验部');
+INSERT INTO auth_group (name) VALUES ('项目管理');
+INSERT INTO auth_group (name) VALUES ('业务员（销售）');
+INSERT INTO auth_group (name) VALUES ('市场部');
+INSERT INTO auth_group (name) VALUES ('财务部');
+INSERT INTO auth_group (name) VALUES ('业务员（公司）');
+INSERT INTO auth_group (name) VALUES ('销售总监');
+INSERT INTO auth_group (name) VALUES ('合作伙伴');
+INSERT INTO auth_group (name) VALUES ('生物信息部');
