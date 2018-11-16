5b1e9dc3e47e1fd4b0f4e095e297cc2e155c0ddc
gmdzy2010 <gmdzy2010@126.com>
2018-11-16 13:27:48 +0800

    李洋-20181116-和云哥一起修复自动补全bug

--- am/views.py
+++ am/views.py
@@ -1,6 +1,6 @@
 from django.contrib.admin.views.autocomplete import AutocompleteJsonView
-from django.contrib.auth.models import Group
-from django.db.models import Q
+from django.contrib.auth.models import Group, User
+from django.db.models import Q, QuerySet
 
 
 class AnaAutocompleteJsonView(AutocompleteJsonView):
@@ -8,24 +8,24 @@ class AnaAutocompleteJsonView(AutocompleteJsonView):
     the group that the request.user in."""
     
     def get_group_users(self, query_condition=None):
-        groups = Group.objects.filter(query_condition)
-        users_queryset = [u for g in groups for u in g.user_set.all()]
-        return users_queryset
+        users_qs = User.objects.filter(query_condition)
+        users_qs, search_use_distinct = self.model_admin.get_search_results(
+            self.request, users_qs, self.term
+        )
+        return users_qs.distinct() if search_use_distinct else users_qs
     
     def get_queryset(self):
         # Attention: the hard code of group should be removed in the future,
         # These may cause some problems when the database are going to be
         # regenerated, last edited by liy, 11/16/2018, 10:41:03.
-        ana_users = self.get_group_users(Q(id=9) | Q(id=10))
-        sal_users = self.get_group_users(Q(id=3) | Q(id=7))
-        exp_users = self.get_group_users(Q(id=1) | Q(id=11))
-        mar_users = self.get_group_users(Q(id=4) | Q(id=12))
-        fin_users = self.get_group_users(Q(id=5) | Q(id=14))
-        
+        ana_users = self.get_group_users(Q(groups__id=9) | Q(groups__id=10))
+        sal_users = self.get_group_users(Q(groups__id=3) | Q(groups__id=7))
+        exp_users = self.get_group_users(Q(groups__id=1) | Q(groups__id=11))
+        mar_users = self.get_group_users(Q(groups__id=4) | Q(groups__id=12))
+        fin_users = self.get_group_users(Q(groups__id=5) | Q(groups__id=14))
         if self.request.user in ana_users:
             users_queryset = ana_users
         elif self.request.user in mar_users:
-            sal_users.extend(mar_users)
             users_queryset = sal_users
         elif self.request.user in sal_users:
             users_queryset = sal_users
