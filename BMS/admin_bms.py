9e9fe84057506f1004e40c126962d576d3f6cb9e
gmdzy2010 <gmdzy2010@126.com>
2018-12-04 21:22:52 +0800

    李洋-20181204-钉钉扫码登陆功能，登陆界面稍作美化

--- BMS/admin_bms.py
+++ BMS/admin_bms.py
@@ -1,15 +1,18 @@
 # -*- coding: UTF-8 -*-
 from django.contrib.admin import AdminSite
 from django.contrib.auth.admin import User, UserAdmin, Group, GroupAdmin
-from django.views.decorators.cache import never_cache
 from django.template.response import TemplateResponse
 from django.utils.translation import ugettext_lazy
-from django.contrib.auth.models import AnonymousUser
+from django.views.decorators.cache import never_cache
+from urllib import parse
+from BMS.settings import DINGTALK_APPID
+
 
 class BMSAdminSite(AdminSite):
-
     site_title = "BMS网站管理"
     site_header = ugettext_lazy('后台管理')
+    login_template = "admin/login.html"
+    
     @never_cache
     def index(self, request, extra_context=None):
         """
@@ -17,17 +20,17 @@ class BMSAdminSite(AdminSite):
         apps that have been registered in this site.
         """
         app_list = self.get_app_list(request)
-
         context = dict(
             self.each_context(request),
             title=self.index_title,
             app_list=app_list,
         )
         context.update(extra_context or {})
-
         request.current_app = self.name
-
-        return TemplateResponse(request, self.index_template or 'admin/index.html', context)
+        return TemplateResponse(
+            request, self.index_template or 'admin/index.html', context
+        )
+    
     def each_context(self, request):
         """
         Returns a dictionary of variables to put in the template context for
@@ -44,7 +47,7 @@ class BMSAdminSite(AdminSite):
         # else:
         #     group_context = 0
         # group_context = [0, ]
-#        if not isinstance(request.user,AnonymousUser):
+        # if not isinstance(request.user,AnonymousUser):
         try:
             groups = Group.objects.filter(user=request.user)
             if groups:
@@ -65,6 +68,28 @@ class BMSAdminSite(AdminSite):
             'group_id' : group_context,
             'available_apps': self.get_app_list(request),
         }
+    
+    @never_cache
+    def login(self, request, extra_context=None):
+        extra_context = extra_context or {}
+        url = "http://{}/dingtalk_auth/".format(request.META["HTTP_HOST"])
+        redirect_uri = parse.quote(url)
+        params = {
+            "appid": DINGTALK_APPID,
+            "response_type": "code",
+            "scope": "snsapi_login",
+            "state": "test",
+            "redirect_uri": redirect_uri,
+        }
+        uri_main = "https://oapi.dingtalk.com/connect/qrconnect"
+        uri_params = "?appid={appid}&response_type={response_type}&" \
+                     "scope={scope}&state={state}&" \
+                     "redirect_uri={redirect_uri}".format(**params)
+        dingtalk_qrcode_uri = "{}{}".format(uri_main, uri_params)
+        extra_context["dingtalk_qrcode_uri"] = dingtalk_qrcode_uri
+        return super().login(request, extra_context=extra_context)
+
+
 BMS_admin_site = BMSAdminSite()
 BMS_admin_site.register(User, UserAdmin)
 BMS_admin_site.register(Group, GroupAdmin)
--- BMS/urls.py
+++ BMS/urls.py
@@ -14,18 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 
-from django.conf.urls import url,include
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from BMS.admin_bms import BMS_admin_site
 from lims.views import getData
+from BMS.views import dingtalk_auth
+
+
 urlpatterns = [
     path(r'', BMS_admin_site.urls),
+    path(r'dingtalk_auth/', dingtalk_auth),
     path(r'notification/inbox/', include('notification.urls', namespace='notifications')),
-    # path(r"lims/display/",)
-    path('lims/getdata/',getData),
+    path('lims/getdata/', getData),
 ]
 
 if settings.DEBUG:
